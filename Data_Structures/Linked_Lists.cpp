ddb5a71d40feb191867b7006ab969b75158181b9
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-04-10 14:54:01 +0530

    Updated Linked_Lists

--- Data_Structures/Linked_Lists.cpp
+++ Data_Structures/Linked_Lists.cpp
@@ -95,13 +95,50 @@ void reverseTheListIteratively(node **headptr){
 		prevNode = currentNode;
 		currentNode = nextNode;
 	}
-	*headptr = prevNode
+	*headptr = prevNode;
 
 }
 
-//Reverse the list recursively
-void reverseTheListRecursively(node **headptr){
+// //Reverse the list recursively
+// void reverseTheListRecursively(node **headptr){
 	
+// }
+
+node *mergeTwoSortedLinkedLists(node *head1,node* head2){
+	node *ptr1 = head1;
+	node *ptr2 = head2;
+	node dummy;
+	dummy.data = -1;
+	dummy.next = NULL;
+	node * result = &dummy;
+
+	while(ptr1 != NULL && ptr2 != NULL){
+        if(ptr1->data < ptr2->data){
+            result->next = ptr1;
+			cout<<"PTR1 node is being added "<<ptr1->data<<"So, tail in result is "<<result->data<<endl;
+            ptr1 = ptr1->next;
+			
+        }
+
+        else{
+            result->next = ptr2;
+            cout<<"PTR2 node is being added "<<ptr2->data<<"So, tail in result is "<<result->data<<endl;
+            ptr2 = ptr2->next;
+        }
+        result = result->next;
+    }
+    while(ptr1 != NULL){
+        result->next = ptr1;
+        ptr1 = ptr1->next;
+        result = result->next;
+    }
+    while(ptr2 != NULL){
+        result->next = ptr2;
+        ptr2 = ptr2->next;
+        result = result->next;
+    }
+    return dummy.next;
+
 }
 
 
@@ -117,24 +154,36 @@ int main()
 	second->next = third;
 	third->data = 3;
 	third->next = NULL;
-	//Expected output : 1 2 3
-	displayLinkedList(head); 
-	insertAtEnd(&head,4);
-	// Expected output : 1 2 3 4
-	displayLinkedList(head); 
-	insertAtHead(&head,0);
-	// Expected output : 0 1 2 3 4
-	displayLinkedList(head);
-	insertAtNthPosition(&head,101,3);
-	// Expected output : 0 1 2 101 3 4
-	displayLinkedList(head);
-	deleteAtNthPosition(&head,3);
-	// Expected output : 0 1 2 3 4
-	displayLinkedList(head);
-	reverseTheListIteratively(&head);
-	// Expected output : 4 3 2 1 0
-	cout<<"Reverse order : ";
-	displayLinkedList(head);
+
+
+	// //Expected output : 1 2 3
+	// displayLinkedList(head); 
+	// insertAtEnd(&head,4);
+	// // Expected output : 1 2 3 4
+	// displayLinkedList(head); 
+	// insertAtHead(&head,0);
+	// // Expected output : 0 1 2 3 4
+	// displayLinkedList(head);
+	// insertAtNthPosition(&head,101,3);
+	// // Expected output : 0 1 2 101 3 4
+	// displayLinkedList(head);
+	// deleteAtNthPosition(&head,3);
+	// // Expected output : 0 1 2 3 4
+	// displayLinkedList(head);
+	// reverseTheListIteratively(&head);
+	// // Expected output : 4 3 2 1 0
+	// cout<<"Reverse order : ";
+	// displayLinkedList(head);
+
+	node * head2 = new node();
+	head2->data = 10;
+	head2->next = NULL;
+	insertAtEnd(&head2,11);
+	insertAtEnd(&head2,12);
+	insertAtEnd(&head2,13);
+	insertAtEnd(&head2,14);
+	node *result = mergeTwoSortedLinkedLists(head,head2);
+	displayLinkedList(result);
     return 0;
 
 }
