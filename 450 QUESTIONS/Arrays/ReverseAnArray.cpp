a336b47907e434fdfc93c76f0595381827c6f224
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-04-27 09:52:32 +0530

    Updated reverseArray

--- 450 QUESTIONS/Arrays/ReverseAnArray.cpp
+++ 450 QUESTIONS/Arrays/ReverseAnArray.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-// void reverse(int arr[],int n){
-
-// }
-
 void printArray(int arr[],int n){
-	cout<<"The elements of the array are : "<<endl;
 	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
+		cout<<arr[i]<<endl;
 	}
 	cout<<endl;
 }
 
-int * getArray(int n){
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cout<<"Enter element at "<<i<<" index  : ";
-		cin>>arr[i];
+void swap(int *p1,int *p2){
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+
+//Reversing array WITHOUT using STACK 
+// Reversing using SWAPPING
+void reverseArray(int arr[],int n){
+	int mid = floor((float)n/2);
+	int i=0, j=n-1,swaps = 0;
+	while(swaps != mid){
+		swap(&arr[i],&arr[j]);
+		i++;
+		j--;
+		swaps ++;
 	}
-	return arr;
 }
 
 int main(){
-	int *arr = getArray(5);
+	int arr[5] = {1,2,3,4,5};
+	cout<<"Elements of the array before reversal  : "<<endl;
 	printArray(arr,5);
+	cout<<"After reversal "<<endl;
+	reverseArray(arr,5);
+	printArray(arr,5);
+	return 0;
 }
